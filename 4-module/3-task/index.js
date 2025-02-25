59b9a1f5525feabd4bb20ee3aadb3cd97c0275b7
Dasha Perova <dashulisaperovaegmail.com>
2025-02-25 13:26:21 +0300

    4 модуль все задачи

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,23 @@
 function highlight(table) {
-  // ваш код...
+  let tbody = table.lastElementChild;
+    for (let i = 0; i < tbody.rows.length; i++) {
+      let row=tbody.rows[i];
+      for (let j = 0; j < row.cells.length; j++) {
+        let td = row.cells[j];
+        if (td.hasAttribute('data-available')) {
+          if (td.getAttribute('data-available') == 'true') {
+            row.classList.add("available");
+          } else row.classList.add("unavailable");
+        } else row.setAttribute('hidden', 'true');
+        if (td.textContent == 'm') {
+          row.classList.add('male');
+        }
+        if (td.textContent == 'f') {
+          row.classList.add('female');
+        }
+        if (Number(td.textContent) && Number(td.textContent) < 18) {
+          row.style = "text-decoration: line-through";
+        }
+      }
+    } 
 }
